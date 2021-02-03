gnu-mirror-unofficial/groff

commit cee547e8839a82f8b21b2859a9370fd74f9793f3

[tbl]: Save and restore hyphenation parameters.
* src/preproc/tbl/table.cpp (table::init_output): When starting a table,
  save the hyphenation parameters (\n[.hy], \n[.hla], \n[.hlm],
  \n[.hym], \n[.hys]).  Restore them in the table reset macro
  (confusingly called "3init"), which is called before performing each
  diversion and before exiting a table.

  This enables people to, e.g., turn off hyphenation in a table text
  block with .nh, just as they can turn off adjustment with .na, without
  having to manually reset it.  The next text block, and the material
  after the table, will not be affected.

Fixes <https://savannah.gnu.org/bugs/?59971>.

src/preproc/tbl/table.cpp
@@ -1,4 +1,4 @@
-/* Copyright (C) 1989-2020 Free Software Foundation, Inc.
+/* Copyright (C) 1989-2021 Free Software Foundation, Inc.
      Written by James Clark (jjc@jclark.com)
 
 This file is part of groff.
@@ -63,6 +63,11 @@ const int DEFAULT_COLUMN_SEPARATION = 3;
 #define ROW_MAX_LINE_REG PREFIX "lnmx"
 #define REPEATED_NM_SET_MACRO PREFIX "rlns"
 #define REPEATED_NM_SUS_MACRO PREFIX "rlnx"
+#define SAVED_HYPHENATION_MODE_REG PREFIX "hyphmode"
+#define SAVED_HYPHENATION_LANG_NAME PREFIX "hyphlang"
+#define SAVED_HYPHENATION_MAX_LINES_REG PREFIX "hyphmaxlines"
+#define SAVED_HYPHENATION_MARGIN_REG PREFIX "hyphmargin"
+#define SAVED_HYPHENATION_SPACE_REG PREFIX "hyphspace"
 
 // this must be one character
 #define COMPATIBLE_REG PREFIX "c"
@@ -1771,6 +1776,11 @@ void table::init_output()
 	 ".in \\n[.i]u\n"
 	 ".ll \\n[.l]u\n"
 	 ".ls \\n[.L]\n"
+	 ".hy \\\\n[" SAVED_HYPHENATION_MODE_REG "]\n"
+	 ".hla \\\\*[" SAVED_HYPHENATION_LANG_NAME "]\n"
+	 ".hlm \\\\n[" SAVED_HYPHENATION_MAX_LINES_REG "]\n"
+	 ".hym \\\\n[" SAVED_HYPHENATION_MARGIN_REG "]\n"
+	 ".hys \\\\n[" SAVED_HYPHENATION_SPACE_REG "]\n"
 	 ".ad \\n[.j]\n"
 	 ".ie \\n[.u] .fi\n"
 	 ".el .nf\n"
@@ -1782,6 +1792,11 @@ void table::init_output()
 	 ".nr " SAVED_SIZE_REG " \\n[.s]\n"
 	 ".nr " SAVED_FILL_REG " \\n[.u]\n"
 	 ".ds " SAVED_TABS_NAME " \\n[.tabs]\n"
+	 ".nr " SAVED_HYPHENATION_MODE_REG " \\n[.hy]\n"
+	 ".ds " SAVED_HYPHENATION_LANG_NAME " \\n[.hla]\n"
+	 ".nr " SAVED_HYPHENATION_MAX_LINES_REG " \\n[.hlm]\n"
+	 ".nr " SAVED_HYPHENATION_MARGIN_REG " \\n[.hym]\n"
+	 ".nr " SAVED_HYPHENATION_SPACE_REG " \\n[.hys]\n"
 	 ".nr T. 0\n"
 	 ".nr " CURRENT_ROW_REG " 0-1\n"
 	 ".nr " LAST_PASSED_ROW_REG " 0-1\n"

commit 49a2c2cab0bbed2f3278a368a2e5d9d593dfc7a9

[tbl]: Fix integer overflow of groff register.
* src/preproc/tbl/table.cpp (table::init_output): Save value of
  hyphenation maximum consecutive line count register (\n[.hlm]) more
  carefully to keep it from being incorrectly parsed as a decrement,
  because negative values of \n[.hlm] are valid (in fact, "-1" is the
  default).  In documents with a large number of tables, this can lead
  to a Persian chessboard problem and an integer overflow (at the groff
  language level, caught and handled by
  src/roff/troff/number.cpp:parse_term()).

  It is wise to wrap a non-literal second argument to the .nr request in
  parentheses if assignment is desired and its value can be negative.
  See section 5.6.1 ("Setting Registers") of the groff Texinfo manual.

Fixes <https://savannah.gnu.org/bugs/?59993>.

Problem introduced by me in commit
cee547e8839a82f8b21b2859a9370fd74f9793f3 on 30 January.

src/preproc/tbl/table.cpp
@@ -1794,7 +1794,7 @@ void table::init_output()
 	 ".ds " SAVED_TABS_NAME " \\n[.tabs]\n"
 	 ".nr " SAVED_HYPHENATION_MODE_REG " \\n[.hy]\n"
 	 ".ds " SAVED_HYPHENATION_LANG_NAME " \\n[.hla]\n"
-	 ".nr " SAVED_HYPHENATION_MAX_LINES_REG " \\n[.hlm]\n"
+	 ".nr " SAVED_HYPHENATION_MAX_LINES_REG " (\\n[.hlm])\n"
 	 ".nr " SAVED_HYPHENATION_MARGIN_REG " \\n[.hym]\n"
 	 ".nr " SAVED_HYPHENATION_SPACE_REG " \\n[.hys]\n"
 	 ".nr T. 0\n"