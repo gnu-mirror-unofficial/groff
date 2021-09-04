f2d7e3ee22ed281beb0d096a773f995b25269a48
G. Branden Robinson <g.branden.robinson@gmail.com>
2021-09-04 22:58:07 +1000

    Add regression test for Savannah #60189.
    * src/roff/groff/tests/break_zero-length_output_line_sanely.sh: Do it.
    * src/roff/groff/groff.am (groff_TESTS): Run test.
    
    Test fails at this commit.

---
+++ src/roff/groff/tests/break_zero-length_output_line_sanely.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# Copyright (C) 2021 Free Software Foundation, Inc.
+#
+# This file is part of groff.
+#
+# groff is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# groff is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+groff="${abs_top_builddir:-.}/test-groff"
+
+set -e
+
+# Do not core dump when attempting to distribute a space amount of zero
+# if someone sets the line length to zero.  See Savannah #61089.
+# Reproducer courtesy of John Gardner.
+
+INPUT='.de _
+.	na
+.	nh
+.	ll 0
+.	di A
+\\&\\\\$1
+.	di
+.	br
+..
+._ " XYZ"
+.A'
+
+OUTPUT=$(echo "$INPUT" | "$groff" -Tascii)
+echo "$OUTPUT" | grep -qx XYZ
