gnu-mirror-unofficial/groff

commit 9d61b3d142842589b90d7eda0ed3270fbbf6166f

[troff]: Enable ASCII in device control escapes.
[troff]: Convert special character glyphs corresponding to Unicode Basic
Latin ("ASCII") code points to those code points when they occur in
device escapes.  (They should be correct for IBM code page 1047 as well,
but this is untested.)  This is necessary for encoding URLs in device
control commands.  Special character identifiers are presumed to be the
defaults documented in groff_char(7); this is a design gap that we
should consider addressing.  (We don't have a way to ask "is this the
special character corresponding to Unicode basic Latin code point X?")

* src/roff/troff/input.cpp (encode_char): Do it.

I'm not documenting this in NEWS as it feels like a pretty dusty corner
even though I'm about to leverage it for something of much higher
visibility.

src/roff/troff/input.cpp
@@ -5414,6 +5414,25 @@ static void encode_char(macro *mac, char c)
     else if (tok.is_stretchable_space()
 	     || tok.is_unstretchable_space())
       mac->append(' ');
+    else if (tok.is_special()) {
+      const char *sc = tok.get_char()->get_symbol()->contents();
+      if (strcmp("-", sc) == 0)
+	mac->append('-');
+      else if (strcmp("aq", sc) == 0)
+	mac->append('\'');
+      else if (strcmp("dq", sc) == 0)
+	mac->append('"');
+      else if (strcmp("ga", sc) == 0)
+	mac->append('`');
+      else if (strcmp("ha", sc) == 0)
+	mac->append('^');
+      else if (strcmp("rs", sc) == 0)
+	mac->append('\\');
+      else if (strcmp("ti", sc) == 0)
+	mac->append('^');
+      else
+	error("special character '%1' cannot be used within \\X", sc);
+    }
     else if (!(tok.is_hyphen_indicator()
 	       || tok.is_dummy()
 	       || tok.is_transparent_dummy()

commit 3d1988cabc90f3c4b0b0000bb4a809be61eeba3c

[troff]: Map \[ti] correctly in \X escapes.
[troff]: Map \[ti] correctly in device control escape sequences.

* src/roff/troff/input.cpp (encode_char): Fix copy-and-paste error.
  \[ti] should put '~', not '^', into a device control command.

Fixes <https://savannah.gnu.org/bugs/?61403>; problem introduced
by me in commit 9d61b3d1, 1 October.

src/roff/troff/input.cpp
@@ -5429,7 +5429,7 @@ static void encode_char(macro *mac, char c)
       else if (strcmp("rs", sc) == 0)
 	mac->append('\\');
       else if (strcmp("ti", sc) == 0)
-	mac->append('^');
+	mac->append('~');
       else
 	error("special character '%1' cannot be used within device"
 	      " control escape sequence", sc);