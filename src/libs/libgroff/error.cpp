gnu-mirror-unofficial/groff

commit 9a03816123eff2f840db444243e5ba0acd0995b4

src/libs/libgroff/error.cpp: Shun heap allocation.
* src/libs/libgroff/error.cpp (do_error_with_file_and_line): Revise to
  eliminate `fprintf()` calls, which might perform dynamic memory
  allocation, rendering this function unsafe to call after memory
  allocation failures.  Since this function is near the top of our
  diagnostic output call stack, that would be unfortunate.  `errprint()`
  does not use dynamic allocation, nor do the `i_to_a()` and `ui_to_a()`
  functions it calls to format integers.

src/libs/libgroff/error.cpp
@@ -41,17 +41,22 @@ static void do_error_with_file_and_line(const char *filename,
 {
   bool need_space = false;
   if (program_name) {
-    fprintf(stderr, "%s:", program_name);
+    fputs(program_name, stderr);
     need_space = true;
   }
   if (filename != 0) {
     if (strcmp(filename, "-") == 0)
       filename = "<standard input>";
-    fprintf(stderr, "%s", filename);
-    if (source_filename != 0)
-      fprintf(stderr, ":(%s)", source_filename);
-    if (lineno > 0)
-      fprintf(stderr, ":%d", lineno);
+    fputs(filename, stderr);
+    if (source_filename != 0) {
+      fputs(":(", stderr);
+      fputs(source_filename, stderr);
+      fputc(')', stderr);
+    }
+    if (lineno > 0) {
+      fputc(':', stderr);
+      errprint("%1", lineno);
+    }
     fputc(':', stderr);
     need_space = true;
   }

commit eb118a6a535b5ac4ba9d6f3d28f29bea084d1970

[libgroff]: Fix missing colon in diag messages.
* src/libs/libgroff/error.cpp (do_error_with_file_and_line): Restore
  missing colon to diagnostic text.  Problem introduced by me in commit
  9a038161, 8 November.

src/libs/libgroff/error.cpp
@@ -42,6 +42,7 @@ static void do_error_with_file_and_line(const char *filename,
   bool need_space = false;
   if (program_name) {
     fputs(program_name, stderr);
+    fputc(':', stderr);
     need_space = true;
   }
   if (filename != 0) {