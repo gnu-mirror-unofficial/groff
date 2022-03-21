gnu-mirror-unofficial/groff

commit 0d451902c070e1224fa5db72f8328f8f5e021385

[afmtodit]: Trivially refactor; apply DRY.
* src/utils/afmtodit/afmtodit.pl: Trivially refactor.  Store the
  program's version string in scalar `afmtodit_version` and use that in
  the usage message and comment embedded in the output.

src/utils/afmtodit/afmtodit.pl
@@ -34,8 +34,10 @@ getopts('a:cd:e:f:i:kmno:svx');
 our ($opt_a, $opt_c, $opt_d, $opt_e, $opt_f, $opt_i, $opt_k,
      $opt_m, $opt_n, $opt_o, $opt_s, $opt_v, $opt_x);
 
+my $afmtodit_version = "GNU afmtodit (groff) version @VERSION@\n";
+
 if ($opt_v) {
-    print "GNU afmtodit (groff) version @VERSION@\n";
+    print "$afmtodit_version";
     exit 0;
 }
 
@@ -422,8 +424,7 @@ foreach my $lig (sort keys %default_ligatures) {
 open(FONT, ">$outfile") || die "$prog: can't open '$outfile' for output: $!\n";
 select(FONT);
 
-print("# This file was generated with " .
-      "GNU afmtodit (groff) version @VERSION@\n");
+print("# This file was generated with $afmtodit_version.\n");
 print("#\n");
 print("#   $fullname\n") if defined $fullname;
 print("#   $version\n") if defined $version;

commit 868fcc5c33a5688341f3f5da286f593723cc0e69

src/utils/afmtodit/afmtodit.pl: Fix version string.
* src/utils/afmtodit/afmtodit.pl: Fix bug introduced in commit
  0d451902c, 10 March.  Don't store a newline in the version string.
  Put `\n` in its interpolation context as necessary.

src/utils/afmtodit/afmtodit.pl
@@ -34,10 +34,10 @@ getopts('a:cd:e:f:i:kmno:svx');
 our ($opt_a, $opt_c, $opt_d, $opt_e, $opt_f, $opt_i, $opt_k,
      $opt_m, $opt_n, $opt_o, $opt_s, $opt_v, $opt_x);
 
-my $afmtodit_version = "GNU afmtodit (groff) version @VERSION@\n";
+my $afmtodit_version = "GNU afmtodit (groff) version @VERSION@";
 
 if ($opt_v) {
-    print "$afmtodit_version";
+    print "$afmtodit_version\n";
     exit 0;
 }
 