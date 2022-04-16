gnu-mirror-unofficial/groff

commit eb4f0675e322d50c69d36625f296f8260fafeb46

[grn]: Perform more input validation.
* src/preproc/grn/hdb.cpp: Perform more input validation.  Improve
  diagnostics by taking advantage of libgroff infrastructure and
  tracking the line number of the input file.  Add global `lineno`.

  (DBRead): Increment `lineno` after reading newlines from input.  Call
  `error_with_file_and_line()` instead of `error()`.  If input reports a
  negative length for the text (string) to follow in the file, exit with
  a fatal diagnostic.  Check for EOF while reading text string.

  (DBGetType): Convert `fprintf()` call for warning diagnostic to
  `warning_with_file_and_line()`.

  (DBRead, DBGetType): Add contextual information to diagnostic
  messages.

Fixes <https://savannah.gnu.org/bugs/?61043>.   Thanks to Savannah user
eqkws for the report.

src/preproc/grn/hdb.cpp
@@ -38,6 +38,7 @@ extern POINT *PTMakePoint(double x, double y, POINT ** pplist);
 
 int DBGetType(register char *s);
 
+long lineno = 0;
 
 /*
  * This routine returns a pointer to an initialized database element which
@@ -97,15 +98,18 @@ DBRead(register FILE *file)
   SUNFILE = FALSE;
   elist = DBInit();
   (void) fscanf(file, "%" MAXSTRING_S "s%*[^\n]\n", string);
+  lineno++;
   if (strcmp(string, "gremlinfile")) {
     if (strcmp(string, "sungremlinfile")) {
-      error("'%1' is not a gremlin file", gremlinfile);
+      error_with_file_and_line(gremlinfile, lineno,
+			       "not a gremlin file");
       return (elist);
     }
     SUNFILE = TRUE;
   }
 
   (void) fscanf(file, "%d%lf%lf\n", &size, &x, &y);
+  lineno++;
   /* ignore orientation and file positioning point */
 
   done = FALSE;
@@ -114,9 +118,11 @@ DBRead(register FILE *file)
     /* I changed the scanf format because the element */
     /* can have two words (e.g. CURVE SPLINE)         */
     if (fscanf(file, "\n%" MAXSTRING_S "[^\n]%*[^\n]\n", string) == EOF) {
-      error("'%1', error in file format", gremlinfile);
+      lineno++;
+      error_with_file_and_line(gremlinfile, lineno, "error in format");
       return (elist);
     }
+    lineno++;
 
     type = DBGetType(string);	/* interpret element type */
     if (type < 0) {		/* no more data */
@@ -126,6 +132,7 @@ DBRead(register FILE *file)
       (void) xscanf(file, &x, &y);		/* always one point */
 #else
       (void) fscanf(file, "%lf%lf\n", &x, &y);	/* always one point */
+      lineno++;
 #endif	/* UW_FASTSCAN */
       plist = PTInit();		/* NULL point list */
 
@@ -146,6 +153,7 @@ DBRead(register FILE *file)
 	lastpoint = FALSE;
 	do {
 	  fgets(string, MAXSTRING, file);
+	  lineno++;
 	  if (string[0] == '*') {	/* SUN gremlin file */
 	    lastpoint = TRUE;
 	  } else {
@@ -180,6 +188,7 @@ DBRead(register FILE *file)
 	  savebounds(nx, y);
 
 	  fgets(string, MAXSTRING, file);
+	  lineno++;
 	  if (string[0] == '*') {	/* SUN gremlin file */
 	    lastpoint = TRUE;
 	  } else {
@@ -191,14 +200,26 @@ DBRead(register FILE *file)
 #endif	/* UW_FASTSCAN */
       }
       (void) fscanf(file, "%d%d\n", &brush, &size);
+      lineno++;
       (void) fscanf(file, "%d", &len);	/* text length */
       (void) getc(file);		/* eat blank */
+      lineno++;				/* advance line counter early */
+      if (len < 0) {
+	fatal_with_file_and_line(gremlinfile, lineno,
+				"length claimed for text is nonsense:"
+				" '%1'", len);
+      }
       txt = (char *) grnmalloc((unsigned) len + 1, "element text");
       for (i = 0; i < len; ++i) {	/* read text */
         int c = getc(file);
         if (c == EOF)
           break;
 	txt[i] = c;
+      }
+      if (feof(file)) {
+	fatal_with_file_and_line(gremlinfile, lineno,
+				 "end of file while reading text of"
+				 " length %1", len);
       }
       txt[len] = '\0';
       (void) DBCreateElt(type, plist, brush, size, txt, &elist);
@@ -234,8 +255,8 @@ DBGetType(register char *s)
       case 'S': 
 	return(BSPLINE);
       case 'E':
-	fprintf(stderr,
-		"Warning: Bezier Curves will be printed as B-Splines\n");
+	warning_with_file_and_line(gremlinfile, lineno,
+				   "using B-spline for Bezier curve");
 	return(BSPLINE);
       default:
 	return(CURVE);
@@ -249,8 +270,9 @@ DBGetType(register char *s)
     case 'R':
       return (CENTRIGHT);
     default:
-      fatal("unknown element type");
-      // fatal() does not return
+      fatal_with_file_and_line(gremlinfile, lineno,
+			       "unknown element type '%1'", s);
+      // fatal_with_file_and_line() does not return
     }
   case 'B':
     switch (s[3]) {
@@ -261,8 +283,9 @@ DBGetType(register char *s)
     case 'R':
       return (BOTRIGHT);
     default:
-      fatal("unknown element type");
-      // fatal() does not return
+      fatal_with_file_and_line(gremlinfile, lineno,
+			       "unknown element type '%1'", s);
+      // fatal_with_file_and_line() does not return
     }
   case 'T':
     switch (s[3]) {
@@ -273,11 +296,13 @@ DBGetType(register char *s)
     case 'R':
       return (TOPRIGHT);
     default:
-      fatal("unknown element type");
-      // fatal() does not return
+      fatal_with_file_and_line(gremlinfile, lineno,
+			       "unknown element type '%1'", s);
+      // fatal_with_file_and_line() does not return
     }
   default:
-    fatal("unknown element type");
+    fatal_with_file_and_line(gremlinfile, lineno,
+			     "unknown element type '%1'", s);
   }
 
   return 0;				/* never reached */

commit 9c4da4d6925431b4969fe01bac8ec4f8e51da4d4

[grn]: Add more input validity checking.
* src/preproc/grn/hdb.cpp (DBRead): Add more validity checking.  Verify
  that the number of conversions returned by fscanf() is as expected
  instead of throwing this information away--abort processing ("giving
  up" like pic(1) does) if it does not.  Consistently report this
  abandonment in diagnostic messages.  Similarly validate pointer
  returned by fgets().  Soften handling of invalid text condition from
  fatal errors, introduced in commit eb4f0675e, 16 August, to throw an
  error and give up.  Addresses "-Wunused-result" warnings from GCC.

src/preproc/grn/hdb.cpp
@@ -96,18 +96,31 @@ DBRead(FILE *file)
 
   SUNFILE = FALSE;
   elist = DBInit();
-  (void) fscanf(file, "%" MAXSTRING_S "s%*[^\n]\n", string);
+  int nitems = fscanf(file, "%" MAXSTRING_S "s%*[^\n]\n", string);
+  if (nitems != 1) {
+    error_with_file_and_line(gremlinfile, lineno,
+			     "malformed input; giving up on this"
+			     " picture");
+    return (elist);
+  }
   lineno++;
   if (strcmp(string, "gremlinfile")) {
     if (strcmp(string, "sungremlinfile")) {
       error_with_file_and_line(gremlinfile, lineno,
-			       "not a gremlin file");
+			       "not a gremlin file; giving up on this"
+			       " picture");
       return (elist);
     }
     SUNFILE = TRUE;
   }
 
-  (void) fscanf(file, "%d%lf%lf\n", &size, &x, &y);
+  nitems = fscanf(file, "%d%lf%lf\n", &size, &x, &y);
+  if (nitems != 3) {
+    error_with_file_and_line(gremlinfile, lineno,
+			     "malformed input; giving up on this"
+			     " picture");
+    return (elist);
+  }
   lineno++;
   /* ignore orientation and file positioning point */
 
@@ -120,7 +133,8 @@ DBRead(FILE *file)
 		      MAXSTRING_S
 		     "[^\n]%*[^\n]\n", string) == EOF) {
       lineno++;
-      error_with_file_and_line(gremlinfile, lineno, "error in format");
+      error_with_file_and_line(gremlinfile, lineno, "error in format;"
+			       " giving up on this picture");
       return (elist);
     }
     lineno++;
@@ -129,10 +143,17 @@ DBRead(FILE *file)
     if (type < 0) {		/* no more data */
       done = TRUE;
     } else {
+      /* always one point */
 #ifdef UW_FASTSCAN
-      (void) xscanf(file, &x, &y);		/* always one point */
+      (void) xscanf(file, &x, &y);
 #else
-      (void) fscanf(file, "%lf%lf\n", &x, &y);	/* always one point */
+      nitems = fscanf(file, "%lf%lf\n", &x, &y);
+      if (nitems != 2) {
+	error_with_file_and_line(gremlinfile, lineno,
+				 "malformed input; giving up on this"
+				 " picture");
+	return (elist);
+      }
       lineno++;
 #endif	/* UW_FASTSCAN */
       plist = PTInit();		/* NULL point list */
@@ -154,14 +175,23 @@ DBRead(FILE *file)
 #else
 	lastpoint = FALSE;
 	do {
-	  fgets(string, MAXSTRING, file);
+	  char *cp = fgets(string, MAXSTRING, file);
+	  if (0 /* nullptr */ == cp) {
+	    error_with_file_and_line(gremlinfile, lineno,
+				     "premature end-of-file or error"
+				     " reading input; giving up on this"
+				     " picture");
+	    return(elist);
+	  }
 	  lineno++;
 	  if (string[0] == '*') {	/* SUN gremlin file */
 	    lastpoint = TRUE;
 	  } else {
 	    if (!sscanf(string, "%lf%lf", &x, &y)) {
-	      error("expected coordinate pair, got '%1';"
-		    " giving up on this picture", string);
+	      error_with_file_and_line(gremlinfile, lineno,
+				       "expected coordinate pair, got"
+				       " '%1'; giving up on this"
+				       " picture", string);
 	      return(elist);
 	    }
 	    if ((x == -1.00 && y == -1.00) && (!SUNFILE))
@@ -189,7 +219,14 @@ DBRead(FILE *file)
 	  (void) PTMakePoint(nx, y, &plist);
 	  savebounds(nx, y);
 
-	  fgets(string, MAXSTRING, file);
+	  char *cp = fgets(string, MAXSTRING, file);
+	  if (0 /* nullptr */ == cp) {
+	    error_with_file_and_line(gremlinfile, lineno,
+				     "premature end-of-file or error"
+				     " reading input; giving up on this"
+				     " picture");
+	    return(elist);
+	  }
 	  lineno++;
 	  if (string[0] == '*') {	/* SUN gremlin file */
 	    lastpoint = TRUE;
@@ -201,15 +238,29 @@ DBRead(FILE *file)
 	}
 #endif	/* UW_FASTSCAN */
       }
-      (void) fscanf(file, "%d%d\n", &brush, &size);
+      nitems = fscanf(file, "%d%d\n", &brush, &size);
+      if (nitems != 2) {
+	error_with_file_and_line(gremlinfile, lineno,
+				 "malformed input; giving up on this"
+				 " picture");
+	return (elist);
+      }
       lineno++;
-      (void) fscanf(file, "%d", &len);	/* text length */
+      nitems = fscanf(file, "%d", &len);	/* text length */
+      if (nitems != 1) {
+	error_with_file_and_line(gremlinfile, lineno,
+				 "malformed input; giving up on this"
+				 " picture");
+	return (elist);
+      }
       (void) getc(file);		/* eat blank */
       lineno++;				/* advance line counter early */
       if (len < 0) {
-	fatal_with_file_and_line(gremlinfile, lineno,
-				"length claimed for text is nonsense:"
-				" '%1'", len);
+	error_with_file_and_line(gremlinfile, lineno,
+				 "length claimed for text is nonsense:"
+				 " '%1'; giving up on this picture",
+				 len);
+	return (elist);
       }
       txt = (char *) grnmalloc((unsigned) len + 1, "element text");
       for (i = 0; i < len; ++i) {	/* read text */
@@ -219,9 +270,11 @@ DBRead(FILE *file)
 	txt[i] = c;
       }
       if (feof(file)) {
-	fatal_with_file_and_line(gremlinfile, lineno,
+	error_with_file_and_line(gremlinfile, lineno,
 				 "end of file while reading text of"
-				 " length %1", len);
+				 " length %1; giving up on this"
+				 " picture", len);
+	return (elist);
       }
       txt[len] = '\0';
       (void) DBCreateElt(type, plist, brush, size, txt, &elist);